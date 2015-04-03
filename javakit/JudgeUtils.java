750cb3f510f6b724725739efd6307de459dbd78f
hisunsray <1305314861@qq.com>
2015-04-03 15:36:10 +0800

    the changed the way the annotation language

--- javakit/JudgeUtils.java
+++ javakit/JudgeUtils.java
@@ -28,14 +28,15 @@ import java.util.Map;
  * @author lizw
  */
 public class JudgeUtils {
+
 	/**
 	 * <p>
-	 * 判断一个对象是否为空 <br>
+	 * to judge whether an object is empty <br>
 	 * 2014-10-23 下午2:41:59
 	 * 
 	 * @param obj
-	 *            要被测试的对象
-	 * @return true: 不为空 false: 为空
+	 *            object to be tested
+	 * @return true: no empty false: empty
 	 */
 	public static boolean isNotEmpty(Object obj) {
 		return null != obj ? true : false;
@@ -43,12 +44,12 @@ public class JudgeUtils {
 
 	/**
 	 * <p>
-	 * 判断一个字符串是否为空 <br>
+	 * checks if a string is empty <br>
 	 * 2014-10-23 下午2:46:18
 	 * 
 	 * @param element
-	 *            要被测试的字符串
-	 * @return true: 不为空 false: 为空
+	 *            the string to be tested
+	 * @return true: no empty false: empty
 	 */
 	public static boolean isNotEmpty(String element) {
 		return null != element && !"".equals(element) ? true : false;
@@ -56,12 +57,12 @@ public class JudgeUtils {
 
 	/**
 	 * <p>
-	 * 判断一个集合对象是否为空 <br>
+	 * to judge whether a collection object is empty <br>
 	 * 2014-11-18 下午4:53:59
 	 * 
 	 * @param list
-	 *            集合对象
-	 * @return true: 不为空 false: 为空
+	 *            the collection object
+	 * @return true: no empty false: empty
 	 */
 	public static boolean isNotEmpty(Collection<?> list) {
 		return null != list && list.size() > 0 ? true : false;
@@ -69,12 +70,12 @@ public class JudgeUtils {
 
 	/**
 	 * <p>
-	 * 判断一个Map<K,V>集合对象是否为空 <br>
+	 * judge a map < k, v > set whether the object is empty <br>
 	 * 2014-11-18 下午4:53:59
 	 * 
 	 * @param map
-	 *            集合对象
-	 * @return true: 不为空 false: 为空
+	 *            the collection object
+	 * @return true: no empty false: empty
 	 */
 	public static boolean isNotEmpty(Map<?, ?> map) {
 		return null != map && map.size() > 0 ? true : false;
