34729e373140dc8de1f9349a84510d025cad7b12
DovSnier <dovsnier@qq.com>
2018-07-14 18:38:43 +0800

    the MVP templates that added with API 2

---
+++ fileTemplates/MVP,M(BaseBean).java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+import com.dvsnier.base.bean.BaseBean;
+
+#parse("MVP File Header.java")
+public class ${NAME} extends BaseBean {
+
+}
---
+++ fileTemplates/MVP,P(BaseCompatPresenter API 2).java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+import com.dvsnier.common.presenter.BaseCompatPresenter;
+
+#parse("MVP File Header.java")
+public class ${NAME} extends BaseCompatPresenter<${Activity_Name}> {
+
+    public void request() {
+        // TODO: ${DATE}
+    }
+}
