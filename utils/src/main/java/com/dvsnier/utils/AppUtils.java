40761c6a84a5a525330f4428b0d906468c60b111
DovSnier <dovsnier@qq.com>
2018-08-06 22:26:43 +0800

    the AppUtils optimization

--- utils/src/main/java/com/dvsnier/utils/AppUtils.java
+++ utils/src/main/java/com/dvsnier/utils/AppUtils.java
@@ -8,24 +8,27 @@ import android.content.pm.PackageManager.NameNotFoundException;
 /**
  * 跟App相关的辅助类
  *
- * @author zhy
+ * @author lzw
  */
 public class AppUtils {
+
+    public static final int DEFAULT_VALUE = -1;
 
     private AppUtils() {
         /* cannot be instantiated */
         throw new UnsupportedOperationException("cannot be instantiated");
-
     }
 
     /**
      * 获取应用程序名称
+     *
+     * @param context {@see Context}
+     * @return App Name
      */
     public static String getAppName(Context context) {
         try {
             PackageManager packageManager = context.getPackageManager();
-            PackageInfo packageInfo = packageManager.getPackageInfo(
-                    context.getPackageName(), 0);
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
             int labelRes = packageInfo.applicationInfo.labelRes;
             return context.getResources().getString(labelRes);
         } catch (NameNotFoundException e) {
@@ -37,20 +40,34 @@ public class AppUtils {
     /**
      * [获取应用程序版本名称信息]
      *
-     * @param context
+     * @param context {@see Context}
      * @return 当前应用的版本名称
      */
     public static String getVersionName(Context context) {
         try {
             PackageManager packageManager = context.getPackageManager();
-            PackageInfo packageInfo = packageManager.getPackageInfo(
-                    context.getPackageName(), 0);
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
             return packageInfo.versionName;
-
         } catch (NameNotFoundException e) {
             e.printStackTrace();
         }
         return null;
     }
 
+    /**
+     * [获取应用程序版本名称信息]
+     *
+     * @param context {@see Context}
+     * @return 当前应用的版本名称
+     */
+    public static int getVersionCode(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DEFAULT_VALUE;
+    }
 }
