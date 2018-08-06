9106a4e8018ed1033e0e1797c1554d04ad13d16f
DovSnier <dovsnier@qq.com>
2018-08-06 22:36:36 +0800

    Fix the setOnClickListener template bug

--- fileTemplates/MVP,XXXViewHolder(BaseViewHolder,OnClickListener).java
+++ fileTemplates/MVP,XXXViewHolder(BaseViewHolder,OnClickListener).java
@@ -25,12 +25,13 @@ public class ${NAME} extends BaseViewHolder<${Bean_Name}> {
     @Override
     public void onBindViewHolder(Context context, int position, ${Bean_Name} bean) {
         super.onBindViewHolder(context, position, bean);
-		// TODO: ${DATE} 
+		// TODO: ${DATE}
     }
 
         @Override
     public void setOnClickListener(IBaseOnClickListener onClickListener) {
         if (null != onClickListener && onClickListener instanceof ${OnClickListener_Name}) {
+            super.setOnClickListener(onClickListener);
             this.onClickListener = (${OnClickListener_Name}) onClickListener;
         }
     }
