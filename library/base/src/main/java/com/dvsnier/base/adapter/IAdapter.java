02c7617d3b33667897c5e6c0b9dc2f5a434b530e
DovSnier <dovsnier@qq.com>
2019-06-20 22:49:14 +0800

    the abstract adapter upgrade that belong to base modules

--- library/base/src/main/java/com/dvsnier/base/adapter/IAdapter.java
+++ library/base/src/main/java/com/dvsnier/base/adapter/IAdapter.java
@@ -6,7 +6,7 @@ import android.support.v7.widget.RecyclerView;
  * an adapter
  * Created by lizw on 2016/4/6.
  */
-public interface IAdapter {
+public interface IAdapter extends IAbstractAdapter {
 
     RecyclerView.Adapter getAdapter();
 }
