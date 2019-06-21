86d86f8c34c70ba69f4e935109f45ba26f3ab33b
DovSnier <dovsnier@qq.com>
2019-06-21 08:45:24 +0800

    the common modules upgrade

---
+++ library/common/src/main/java/com/dvsnier/common/callback/ICallback.java
@@ -0,0 +1,8 @@
+package com.dvsnier.common.callback;
+
+/**
+ * ICallback
+ * Created by dovsnier on 2017/1/8.
+ */
+public interface ICallback {
+}
---
+++ library/common/src/main/java/com/dvsnier/common/property/IValue.java
@@ -0,0 +1,8 @@
+package com.dvsnier.common.property;
+
+/**
+ * IValue
+ * Created by dovsnier on 2017/1/8.
+ */
+public interface IValue {
+}
